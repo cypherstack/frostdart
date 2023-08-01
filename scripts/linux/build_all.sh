734658e8db7f01a8c39fd848d03ee96945d07333
julian <julian@cypherstack.com>
2023-08-01 16:50:23 -0600

    WIP test build scripts

---
+++ scripts/linux/build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT_DIR="$(pwd)/../.."
+
+mkdir -p build
+
+
+cd "$ROOT_DIR"/src/serai/hrf || exit
+if [ "$IS_ARM" = true ]  ; then
+    echo "Building arm frostdart"
+    cargo build --target aarch64-unknown-linux-gnu --release --lib
+    cp ../target/aarch64-unknown-linux-gnu/release/libfrostdart.so "$ROOT_DIR"/scripts/linux/build/frostdart.so
+else
+    echo "Building x86_64 frostdart"
+    cargo build --target x86_64-unknown-linux-gnu --release --lib
+    cp ../target/x86_64-unknown-linux-gnu/release/libfrostdart.so "$ROOT_DIR"/scripts/linux/build/frostdart.so
+fi
---
+++ scripts/linux/regen_bindings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../..
+
+dart run ffigen --config ffigen.yaml
