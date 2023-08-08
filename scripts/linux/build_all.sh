835c1e404bf57d9021464563334f49420bd9c049
julian <julian@cypherstack.com>
2023-08-08 10:02:29 -0600

    update build script to use rust 1.71.0

--- scripts/linux/build_all.sh
+++ scripts/linux/build_all.sh
@@ -9,10 +9,10 @@ rm -rf "$ROOT_DIR"/src/serai/target
 cd "$ROOT_DIR"/src/serai/hrf || exit
 if [ "$IS_ARM" = true ]  ; then
     echo "Building arm frostdart"
-    cargo build --target aarch64-unknown-linux-gnu --release --lib
+    cargo +1.71.0 build --target aarch64-unknown-linux-gnu --release --lib
     cp ../target/aarch64-unknown-linux-gnu/release/libhrf_api.so "$ROOT_DIR"/scripts/linux/build/frostdart.so
 else
     echo "Building x86_64 frostdart"
-    cargo build --target x86_64-unknown-linux-gnu --release --lib
+    cargo +1.71.0 build --target x86_64-unknown-linux-gnu --release --lib
     cp ../target/x86_64-unknown-linux-gnu/release/libhrf_api.so "$ROOT_DIR"/scripts/linux/build/frostdart.so
 fi
