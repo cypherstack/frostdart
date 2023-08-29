e81e2d89196f3a7d903759ff4060091703a2969d
julian-CStack <julian@cypherstack.com>
2023-08-29 08:52:52 -0600

    WIP macos build pt2

--- lib/frostdart.dart
+++ lib/frostdart.dart
@@ -12,7 +12,8 @@ const String _libName = 'frostdart';
 /// The dynamic library in which the symbols for [FrostdartBindings] can be found.
 final ffi.DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
-    return ffi.DynamicLibrary.open('$_libName.framework/$_libName');
+    return ffi.DynamicLibrary.process();
+    // return ffi.DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
     return ffi.DynamicLibrary.open('$_libName.so');
--- scripts/macos/build_all.sh
+++ scripts/macos/build_all.sh
@@ -12,4 +12,7 @@ cd "$ROOT_DIR"/src/serai/hrf || exit
 
 echo "Building macos frostdart"
 cargo lipo --release --targets aarch64-apple-darwin,x86_64-apple-darwin
-cp ../target/universal/release/libhrf_api.a "$ROOT_DIR"/macos/frostdart.a
+mkdir -p build "$ROOT_DIR"/macos/libs
+#mkdir -p build "$ROOT_DIR"/macos/include
+cp ../target/universal/release/libhrf_api.a "$ROOT_DIR"/macos/libs/libfrostdart.a
+#cp header.h "$ROOT_DIR"/macos/include/header.h
