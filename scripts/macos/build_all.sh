94770a4ec6dc4190311e562cc1edd33cdc6a0427
Likho <likhojiba@gmail.com>
2023-08-30 20:16:57 +0200

    Correctly check os platform

--- scripts/macos/build_all.sh
+++ scripts/macos/build_all.sh
@@ -11,6 +11,10 @@ rm -rf "$ROOT_DIR"/src/serai/target
 cd "$ROOT_DIR"/src/serai/hrf || exit
 
 echo "Building macos frostdart"
-cargo lipo --release --targets aarch64-apple-darwin
+cargo lipo --release --targets aarch64-apple-darwin,x86_64-apple-darwin
 
-cp ../target/aarch64-apple-darwin/release/libhrf_api.dylib "$ROOT_DIR"/macos/frostdart.dylib
+if [[ $(uname -m) == 'arm64' ]]; then
+    cp ../target/aarch64-apple-darwin/release/libhrf_api.dylib "$ROOT_DIR"/macos/frostdart.dylib
+else
+    cp ../target/x86_64-apple-darwin/release/libhrf_api.dylib "$ROOT_DIR"/macos/frostdart.dylib
+fi
